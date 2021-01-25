c6d38b2d0361983aa4d30e5f59d3ae5f8f0051eb
schalksa <salearncode@gmail.com>
2021-01-25 15:12:27 +0100

    Displaying Name and Habitat

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,7 +3,17 @@ import React from 'react';
 const ListItem = ({ item }) => {
     return (
         <div>
-            <h1>{item.Availability}</h1>
+            {/* Solution for html in my data: <div dangerouslySetInnerHTML={html} /> .... Dangerous because you can access xss, malitious code...*/}
+            <div>
+                <h3>Species Name: </h3>
+                {/* CURLY BRACES: first curly braces: this will be JS, second curly braces: this will an object. */}
+                <div dangerouslySetInnerHTML={{ __html: item["Species Name"] }} />
+            </div>
+            <div>
+                <div>Habitat: </div>
+                <div dangerouslySetInnerHTML={{ __html: item["Habitat"] }} />
+            </div>
+            
         </div>
     )
 }
